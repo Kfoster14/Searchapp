ef9e82fbf85456499cbdd23c3f2fae75b09f8d77
Kfoster14 <kelleyafoster@hotmail.com>
2021-08-25 17:57:08 +1000

    added components folder and files

---
+++ src/components/Messages.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Message from './Message';
+const Messages = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Messages
